b1bc61dc6bc39973f228f6c3c3d577f4689e2297
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-05-24 11:40:54 +0900

    chap1_7_2

---
+++ SQLドリル/chap1/chap1-7/chap1_7_2.sql
@@ -0,0 +1,15 @@
+SELECT
+	l_name,
+	f_name,
+	prefecture
+FROM
+	usr
+WHERE 
+		prefecture = '東京都'
+	OR
+		prefecture = '神奈川県'
+	OR
+		prefecture = '千葉県'
+ORDER BY 
+	l_name_kana ASC,
+	f_name_kana ASC
