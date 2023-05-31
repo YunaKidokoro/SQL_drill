fa8035ac7503f76569fc727934530b16b6053b87
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-05-31 09:44:53 +0900

    chap3_6_3

---
+++ SQLドリル/chap3/chap3_6/chap3_6_3.sql
@@ -0,0 +1,18 @@
+UPDATE
+	books
+SET
+		title
+	=
+		CONCAT(
+			title,
+			(
+			SELECT
+				category_name
+			FROM
+				category
+			WHERE
+				books.category_id
+				= category.category_id
+			)
+		)
+;
